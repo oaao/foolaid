6a1d1c5761411927e91cb871a77d1e53d47eb47b
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-12-11 14:12:33 -0700

    add [fc/list]: chainable reduce

--- foolaid/fcollections/list.py
+++ foolaid/fcollections/list.py
@@ -2,6 +2,8 @@ from forbiddenfruit import curses
 
 from typing import Callable
 
+from functools import reduce
+
 
 @curses(list, 'map')
 def list_map(self, f: Callable) -> list:
@@ -24,3 +26,19 @@ def list_filter(self, f: Callable) -> list:
     :param f: filter function to apply to list
 
     :return: a new list with filter function applied
+    """
+
+    return list(filter(f, self))
+
+
+@curses(list, 'reduce')
+def list_reduce(self, f: Callable) -> list:
+    """
+    Call reduce() as a list method.
+
+    :param f: reduce function to apply to list
+
+    :return: a new list with reduce function applied
+    """
+
+    return list(reduce(f, self))
