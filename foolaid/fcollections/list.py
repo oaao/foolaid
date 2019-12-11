8824a02e139c8c7bcb87f6a92566a9c2279e8cd4
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-12-11 13:46:19 -0700

    add [fc/list]: chainable map method

---
+++ foolaid/fcollections/list.py
@@ -0,0 +1,16 @@
+from forbiddenfruit import curses
+
+from typing import Callable
+
+
+@curses(list, 'map')
+def list_map(self, f: Callable) -> list:
+    """
+    Call map() as a list method.
+
+    :param f: map function to apply to list
+
+    :return: a new list with map function applied
+    """
+
+    return list(map(f, self))
